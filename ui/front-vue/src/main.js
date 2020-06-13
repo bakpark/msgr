553d5768d814ec13a5c876ffb641a3b8f89bfb34
bakpark <toloview@naver.com>
2020-06-13 19:55:44 +0900

    Modifying setting for socket

--- ui/front-vue/src/main.js
+++ ui/front-vue/src/main.js
@@ -3,13 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from './axios';
-import VueNativeSock from 'vue-native-websocket'
-
-Vue.use(VueNativeSock, 'ws://192.168.0.100:8080', {
-    connectManually: true
-})
-
-
 Vue.config.productionTip = false;
 let vm = new Vue({
     router,
@@ -17,6 +10,3 @@ let vm = new Vue({
     axios,
     render: h => h(App)
 }).$mount("#app")
-
-vm.$connect()
-vm.$connect('ws://192.168.0.100:8080/socket', { format: 'json' })
